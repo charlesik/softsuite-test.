b9737afb40dde4a84d729033aade8fe31cbfb661
charlesik <charlesikilo@gmail.com>
2024-01-30 18:26:21 +0100

    topbar component/completed

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -16,8 +16,20 @@ function TopBar() {
             </div>
         </div>
        <Search/>
-       <div><img src="notification.png" alt="" /></div>
+       <div className="bell"><img src="notification.png" alt="" /></div>
+       <div className="switch org userdetails">
+            <div className="switchimg">
+                <img src="user.png" alt="" />
+            </div>
+            <div>
+            <p>Henry Okoro</p> 
+            <p className="bold">Payroll Manager</p>
+            </div>
+            <div className="dropdown">
+            </div>
         </div>
+        </div>
+    
     );
 }
 
