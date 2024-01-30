b7ba23b261f63e4a1192edd7ed041f9cd72864a6
charlesik <charlesikilo@gmail.com>
2024-01-30 18:16:54 +0100

    search component/completed

---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+
+function Search() {
+    return (
+        <div className="searchcomp">
+        <input type="text" placeholder="Search" className="search"/>
+        <button className="searchbtn"><img src="search.png" alt="" /></button>
+    </div>
+    );
+}
+
+export default Search;
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,22 @@
+import Search from "./Search";
 
 function TopBar() {
     return (
         <div className="topbar">
-            poppp
+           <div className="switch org">
+            <div className="switchimg">
+                <img src="Home.png" alt="" />
+            </div>
+            <div>
+            <p>Change Organisation</p> 
+            <p className="bold">Papersoft Limited</p>
+            </div>
+            <div className="dropdown">
+            <img src="dropdown.png" alt="" />
+            </div>
+        </div>
+       <Search/>
+       <div><img src="notification.png" alt="" /></div>
         </div>
     );
 }
